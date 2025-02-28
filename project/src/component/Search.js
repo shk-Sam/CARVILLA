7ff1de34c57e491a08d46a92a99af7870bf52523
shk-Sam <shaikhsaad78890@gmail.com>
2025-02-28 23:13:16 +0530

    adjustment

--- project/src/component/Search.js
+++ project/src/component/Search.js
@@ -14,7 +14,7 @@
 //                 return res.json();
 //             })
 //             .then((res) => {
-//                 // setResult(res)
+//                 query(res)
 //                 console.log(res)
 //             })
 //             .catch((error) => { console.log(error) })
@@ -22,16 +22,16 @@
 //     return (
 //         <>
 //         <h1 style={{backgroundColor:'black', color:'white', display:'flex', justifyContent:'center', margin:'150px 200px 150px 200px', padding:'15px'}}>{query}</h1>
-//         {result.lenght == 0 } 
+//         {/* {result.lenght == 0 }  */}
 //         <div>
 //             <ul>
-//                 {result.map (()=>{
+//                 {/* {result.map (()=>{
 //                 <li>
 //                     <h1></h1>
 //                     <p>price</p>
 //                     <p>desc</p>
 //                 </li>                       
-//                  })} 
+//                  })}  */}
 //             </ul>
 //         </div>
 //         </>
